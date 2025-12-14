92cbde6d7713ff62b5db1858659acc004634fe18
Ultimate <52617311+Ult1mat3S@users.noreply.github.com>
2025-12-14 01:22:38 -0600

    fix: typos

--- src/Support.jsx
+++ src/Support.jsx
@@ -2,7 +2,13 @@ import BMCLogoDark from "../static/bmc-dark.png";
 import BMCLogoLight from "../static/bmc-light.png";
 
 function Support({ darkMode }) {
-  return <img src={darkMode ? BMCLogoDark : BMCLogoLight} alt="BMC Logo" className="w-auto h-10 align-right" />;
+  return (
+    <>
+      <a href="https://buymeacoffee.com/ult1mat3s" target="_blank" rel="noopener noreferrer">
+        <img src={darkMode ? BMCLogoDark : BMCLogoLight} alt="BMC Logo" className="w-auto h-10 align-right" />;
+      </a>
+    </>
+  );
 }
 
 export default Support;
